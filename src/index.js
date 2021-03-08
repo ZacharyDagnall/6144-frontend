d0fc6dd65804dbc32e706b143fca7bab5a9967da
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-03-08 13:06:49 -0500

    swipe right lol

--- src/index.js
+++ src/index.js
@@ -20,6 +20,9 @@ function loadBoard(board) {  //render board (new or updated)
         row.forEach((col, j) => {
             let htmlCol = htmlRow.querySelector(`[col-id="${j}"]`)
             htmlCol.textContent = col
+            if (col === 0) {
+                htmlCol.classList.add("blank")
+            }
         })
     })
 }
@@ -29,7 +32,7 @@ function loadScore(score) {
 }
 
 function fetchBoard() {
-    fetch(`http://localhost:3000/games/1`)
+    fetch(`http://localhost:3000/games/2`)
         .then(r => r.json())
         .then(game => {
             loadBoard(game.board_state)
@@ -39,12 +42,16 @@ function fetchBoard() {
 
 document.addEventListener("keydown", event => {
     if (event.key === "ArrowUp") {
+        event.preventDefault()
         swipeUp()
     } else if (event.key === "ArrowDown") {
+        event.preventDefault()
         swipeDown()
     } else if (event.key === "ArrowLeft") {
+        event.preventDefault()
         swipeLeft()
     } else if (event.key === "ArrowRight") {
+        event.preventDefault()
         swipeRight()
     }
 })
@@ -59,9 +66,48 @@ function swipeLeft() {
     console.log("swiped left!")
 }
 function swipeRight() {
-    console.log("swiped righ!")
+    console.log("swiped right!")
+    // find dom board
+    for (let i = 0; i < 4; i++) {
+        for (let j = 3; j >= 0; j--) {
+            moveRight(i, j)
+        }
+    }
+    // figure out what the new board should be
+    // patch (fetch)
+    // loadBoard()
 }
 
+function moveRight(i, j) {
+    if (j !== 3) {
+        let htmlTile = document.querySelector(`[row-id="${i}"]`).querySelector(`[col-id="${j}"]`)
+        let nextTile = document.querySelector(`[row-id="${i}"]`).querySelector(`[col-id="${j + 1}"]`)
+        console.log(htmlTile)
+        console.log(nextTile)
+
+        if (nextTile.textContent === "0") {
+            nextTile.textContent = htmlTile.textContent
+            nextTile.classList.remove("blank")
+            htmlTile.textContent = 0
+            // htmlTile.classList.add("blank")
+            moveRight(i, j + 1)
+        } else {
+            //smush
+        }
+    }
+    blankZeroes()
+}
+
+function blankZeroes() {
+    // tiles = document.querySelectorAll('.tile')
+    console.log(tiles)
+    tiles.forEach(tile => {
+        if (tile.textContent === "0") {
+            tile.classList.add("blank")
+        }
+    })
+}
 
 makeBoard()
 fetchBoard() // blank or most recent
+const tiles = document.querySelectorAll('.tile')
