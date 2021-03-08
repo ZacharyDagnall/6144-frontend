c6475d4e4e40305cb520dad6cb20c3879b939ce4
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-03-08 11:36:41 -0500

    so far

--- src/index.js
+++ src/index.js
@@ -1,20 +1,68 @@
-// document when loaded
-// // load board
-// // load score
-// // 
 
+document.addEventListener("DOMContentLoaded", event => {
+    makeBoard()
+    //loadScore()
+    fetchBoard() // blank or most recent
+})
 
+const boardDiv = document.querySelector('#board')
+function makeBoard() {  //create HTML items on document
+    for (let i = 0; i < 4; i++) {
+        const row = document.createElement('div')
+        row.classList.add("row")
+        row.setAttribute("row-id", i)
+        boardDiv.append(row)
+        for (let j = 0; j < 4; j++) {
+            let tile = document.createElement('div')
+            tile.setAttribute("col-id", j)
+            tile.classList.add("tile")
+            row.append(tile)
+        }
+    }
+}
+function loadBoard(board) {  //render board (new or updated)
+    let tiles = document.querySelectorAll('.tile')
+    board.forEach(row => {
 
+        row.forEach(col => {
 
-// document listener keypress
-// // if up
-// // // swipeUp()
-// // if down 
-// // // swipeDown()
-// // etc
+        })
+    })
+}
+function loadScore() {
 
-// function swipeUp() {}
+}
 
-// function swipeDown() {}
+function fetchBoard() {
+    fetch(`http://localhost:3000/games/1`)
+        .then(r => r.json())
+        .then(game => {
+            loadBoard(game.board_state)
+            //loadscore
+        })
+}
 
-// etc
+document.addEventListener("keydown", event => {
+    if (event.key === "ArrowUp") {
+        swipeUp()
+    } else if (event.key === "ArrowDown") {
+        swipeDown()
+    } else if (event.key === "ArrowLeft") {
+        swipeLeft()
+    } else if (event.key === "ArrowRight") {
+        swipeRight()
+    }
+})
+
+function swipeUp() {
+    console.log("swiped up!")
+}
+function swipeDown() {
+    console.log("swiped down!")
+}
+function swipeLeft() {
+    console.log("swiped left!")
+}
+function swipeRight() {
+    console.log("swiped righ!")
+}
